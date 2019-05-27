be8542dc98625feb3cbb4b2d1635ce51caf45591
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-05-27 17:00:34 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,7 @@
 # Build your say_hello method here
 # say_hello("Gabriela")
 
-def say_hello(name)
-If name =! "Gabriela"
-  return "Hello Ruby Programmer!"
-else return "Hello Gabriela"
-end
+def say_hello(name = "Ruby Programmer")
+  puts"Hello #{name}!"
 end
 
